60e83a4573067968d29a51c5edf552484c2fadab
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2023-07-03 00:13:52 +0200

    struct content

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# regexor
+# Regexor
+A Regex Validator, made with Rust.  
 Convert help/man text to their regex counterpart. Use this translation to validate command-line
+
+## Install
+`curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
 
 ## Build
 `cargo run -- param1 path/to/file`
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 
+mod structs;
+use crate::structs::Cli;
 use clap::Parser;
-
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
-}
 
 fn main() {
     let cli_arg = Cli::parse();
-
-    // display cliArg properties
-    // println!("{:?}", cliArg);
-
-    println!("called with {}/{}", cli_arg.pattern, cli_arg.path.display());
+    let mut regex_out = String::from("");
+    
+    println!("{:?}", cli_arg.format);
+    for (_i, c) in cli_arg.input.chars().enumerate() {
+        print!("{c}");
+        regex_out.push(c);
+    }
+    
+    println!();
+    //println!("called with {:?}", cli_arg.pattern);
 
 }
